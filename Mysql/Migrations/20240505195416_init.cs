ded1c62952d34e2694f9fc708c0abbcda286cc8e
olegnizh <olegnizh@mail.ru>
2024-06-05 15:30:50 +0300

    Добавьте файлы проекта.

---
+++ Mysql/Migrations/20240505195416_init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mysql.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Qas",
+                columns: table => new
+                {
+                    QaId = table.Column<int>(type: "int(11)", nullable: false),
+                    Quest = table.Column<string>(type: "nvarchar(1000)", nullable: false),
+                    Answ = table.Column<string>(type: "nvarchar(1000)", nullable: true),
+                    Hyper = table.Column<string>(type: "nvarchar(1000)", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: true),
+                    UserMessage = table.Column<string>(type: "text", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<int>(type: "int", nullable: true),
+                    StatusMessage = table.Column<string>(type: "text", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Active = table.Column<int>(type: "int", nullable: false),
+                    Org = table.Column<string>(type: "nvarchar(300)", nullable: true),
+                    Tel = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Qas", x => x.QaId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Qas");
+        }
+    }
+}
---
+++ Mysql/Models/Qa.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mysql.Models
+{
+    public class Qa
+    {
+        //[MaxLength(50)]
+        [Column(TypeName = "int(11)", Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int QaId { get; set; }
+        [Column(TypeName = "nvarchar(1000)", Order = 2)]
+        public string Quest { get; set; }
+        [Column(TypeName = "nvarchar(1000)", Order = 3)]
+        public string? Answ { get; set; }
+        [Column(TypeName = "nvarchar(1000)", Order = 4)]
+        public string? Hyper { get; set; }
+        [Column(Order = 5)]
+        public int? Number { get; set; }
+        [Column(TypeName = "text", Order = 6)]
+        public string? UserMessage { get; set; }
+        [Column(Order = 7)]
+        public int? Status { get; set; }
+        [Column(TypeName = "text", Order = 8)]
+        public string? StatusMessage { get; set; }
+        [Column(Order = 9)]
+        public int Active { get; set; }
+        [Column(TypeName = "nvarchar(300)", Order = 10)]
+        public string? Org { get; set; }
+        [Column(TypeName = "nvarchar(100)", Order = 11)]
+        public string? Tel { get; set; } 
+
+    }
+}
